678b558af92e207618c2f345d688fd05b5cc9fbc
John Wypick <johnwypick@gmail.com>
2018-02-06 04:51:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(string) {
+  return "Happy holidays, you!"
+}
+function happyHolidayTo(string) {
+  return 'Happy ${Holiday}, ${name}!'
+}
